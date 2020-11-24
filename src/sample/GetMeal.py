75b74e19880736953f449f4e81838cf4ead6bc41
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-24 18:07:58 +0100

    added more tests for GetMeal

--- src/sample/GetMeal.py
+++ src/sample/GetMeal.py
@@ -6,7 +6,7 @@ class GetMeal:
         payload = {"s": name}
         response = r.get("https://www.themealdb.com/api/json/v1/1/search.php", params=payload)
         if response.json()["meals"] == None:
-            return print("This meal doesn't exist!")
+            return "This meal doesn't exist!"
         name = response.json()["meals"][0]["strMeal"]
         category = response.json()["meals"][0]["strCategory"]
         instructions = response.json()["meals"][0]["strInstructions"]
--- tests/test_get_meal.py
+++ tests/test_get_meal.py
@@ -8,9 +8,21 @@ class TestSimple(unittest.TestCase):
     def setUp(self):
         self.temp = GetMeal()
 
-    def test_add_one(self):
+    def test_get_meal_positive_arrabiata(self):
         expected = self.temp.getter("Arrabiata")
         self.assertEqual(len(expected), 1)
+
+    def test_get_meal_positive_spaghetti(self):
+        expected = self.temp.getter("Spaghetti")
+        self.assertEqual(len(expected), 1)
+    
+    def test_get_meal_error_str(self):
+        expected = self.temp.getter("Arabarabrara")
+        self.assertEqual(expected, "This meal doesn't exist!")
+    
+    def test_get_meal_error_num(self):
+        expected = self.temp.getter(32131)
+        self.assertEqual(expected, "This meal doesn't exist!")
 
 if __name__ == '__main__':
     unittest.main()
